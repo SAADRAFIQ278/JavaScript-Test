2f59098af3393e62a7170de022fcf75a3102c9ed
SAADRAFIQ278 <mrk544939@gmail.com>
2026-09-13 21:58:04 -0700

    initial msg

---
+++ Test_01/test_01.js
@@ -0,0 +1,55 @@
+// let price = 1200;
+// let quantity = 3;
+
+// let totalBill = price * quantity ;
+
+// console.log(totalBill);
+
+
+// let temperature = 38;
+// if (temperature > 30) {
+//     console.log("Temperature is Hot !");
+// } else {
+//     console.log("Temperature is Normal !");
+
+// }
+
+
+// let number = [4, 7, 12, 15, 20, 23, 30];
+// for (let i = 0; i < number.length; i++) {
+//     if (number[i] % 15 === 0) {
+//         console.log(number[i]);
+
+//     }
+// }
+
+
+// let number = [8, 13, 21, 4, 17, 30, 11];
+// let largestNumber = number[0]
+// for (let i = 0; i < number.length; i++) {
+//     if (number[i] > largestNumber) {
+//         largestNumber = number[i]
+//     }
+
+// } console.log("Largest number is :", largestNumber);
+
+
+// let number = [12, 7, 25, 18, 30, 9, 21];
+// let sum = 0;
+// for (let i = 0; i < number.length; i++) {
+//     if (number[i] > 20) {
+//         sum += number[i]
+//     }
+// } console.log(sum);
+
+// let number = [12, 5, 18, 7, 25, 10];
+// let smallest = number[0];
+// for (let i = 0; i < number.length; i++) {
+//     if (smallest > number[i]) {
+//         smallest = number[i]
+//     }
+// } console.log("Smallest number is :", smallest);
+
+
+
+
